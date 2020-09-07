9daba1245951b790530bf72a10177f317466546c
jueunham <51067762+jueunham@users.noreply.github.com>
2020-09-07 19:28:28 +0900

    2020/09/07 chap2

--- src/chap02/PrimeNumber1.java
+++ src/chap02/PrimeNumber1.java
@@ -1,5 +1,22 @@
 package chap02;
-
+//20.09.07
+// 1000이하의 소수를 나열하는 프로그램(버전1)
 public class PrimeNumber1 {
+	public static void main(String[] args) {
+		int counter = 0; //나눗셈의 횟수
+		
+		for(int n = 2; n <=1000; n++) {
+			int i;
+			for(i =2; i<n; i++) {
+				counter++;
+				if(n % i == 0) // 나누어 떨어지면 소수가 아님
+					break; //반복 나가기
+			}
+			if(n==i) // 마지막까지 나누어떨어지지않음  => 소수
+				System.out.println(n);
+			}
+	
+			System.out.println("나눗셈을 수행한 횟수: " + counter);
+	}
 
 }
